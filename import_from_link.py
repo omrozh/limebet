6729abc4551d981f10d0ca20de78f51364495cb4
omrozh <53612475+omrozh@users.noreply.github.com>
2024-02-19 23:39:48 +0300

    LaLiga & Bundesliga

--- import_from_link.py
+++ import_from_link.py
@@ -3,48 +3,61 @@ from selenium.webdriver.common.by import By
 import os
 
 browser = webdriver.Chrome()
-browser.get(input("Import Team URL: "))
-team_name = input("Team name: ")
+file = open("team-import", "r")
+data = file.read()
 
-elem = browser.find_elements(By.CLASS_NAME, 'odd')
-elem2 = browser.find_elements(By.CLASS_NAME, 'even')
+for c in data.split("\n"):
+    browser.get(c.split(" ")[0])
+    input("Proceed? ")
+    team_name = c.split(" ")[1]
 
-elem = zip(elem, elem2)
+    elem = browser.find_elements(By.CLASS_NAME, 'odd')
+    elem2 = browser.find_elements(By.CLASS_NAME, 'even')
 
-elem = [item for pair in elem for item in pair]
+    elem = zip(elem, elem2)
 
-images = browser.find_elements(By.CLASS_NAME, "bilderrahmen-fixed")
+    elem = [item for pair in elem for item in pair]
 
-elem.extend(elem2)
+    images = browser.find_elements(By.CLASS_NAME, "bilderrahmen-fixed")
 
-players = []
+    elem.extend(elem2)
 
-for i in range(len(elem)):
-    try:
-        players.append({
-            "name": elem[i].text.split("\n")[1].replace(" ", "-").replace("'", "-"),
-            "price": float(elem[i].text.split("\n")[3].split(" ")[4])*100 if "mil" in elem[i].text.split("\n")[3].split(" ")[5] else elem[i].text.split("\n")[3].split(" ")[4],
-            "team": team_name,
-            "image_url": images[i].get_attribute("src")
-        })
-    except IndexError as e:
+    players = []
+
+    for i in range(len(elem)):
         try:
             players.append({
                 "name": elem[i].text.split("\n")[1].replace(" ", "-").replace("'", "-"),
-                "price": float(elem[i].text.split("\n")[4].split(" ")[0])*100 if "mil" in elem[i].text.split("\n")[4] else float(elem[i].text.split("\n")[4].split(" ")[0])/10,
+                "price": float(elem[i].text.split("\n")[3].split(" ")[4])*100 if "mil" in elem[i].text.split("\n")[3].split(" ")[5] else elem[i].text.split("\n")[3].split(" ")[4],
                 "team": team_name,
                 "image_url": images[i].get_attribute("src")
             })
-        except IndexError:
-            break
+        except:
+            try:
+                players.append({
+                    "name": elem[i].text.split("\n")[1].replace(" ", "-").replace("'", "-"),
+                    "price": float(elem[i].text.split("\n")[4].split(" ")[0])*100 if "mil" in elem[i].text.split("\n")[4] else float(elem[i].text.split("\n")[4].split(" ")[0])/10,
+                    "team": team_name,
+                    "image_url": images[i].get_attribute("src")
+                })
+            except:
+                try:
+                    players.append({
+                        "name": elem[i].text.split("\n")[1].replace(" ", "-").replace("'", "-"),
+                        "price": 100,
+                        "team": team_name,
+                        "image_url": images[i].get_attribute("src")
+                    })
+                except:
+                    break
 
-print(str(len(players)) + " athletes added")
-for i in players:
-    print(f"{i.get('name')}: {i.get('price')}₺")
-input("Confirm: ")
+    print(str(len(players)) + " athletes added")
+    for i in players:
+        print(f"{i.get('name')}: {i.get('price')}₺")
+    input("Confirm: ")
 
-os.system(f"python util.py add-team {team_name} {input('Team Image URL: ')}")
+    os.system(f"python util.py add-team {team_name} {c.split(' ')[-1]}")
 
-for i in players:
-    os.system(f"python util.py add-athlete {i.get('name')} {int(i.get('price'))} {i.get('team')}")
-    os.system(f"python util.py add-image {i.get('name')} {i.get('image_url')}")
+    for i in players:
+        os.system(f"python util.py add-athlete {i.get('name')} {int(i.get('price'))} {i.get('team')}")
+        os.system(f"python util.py add-image {i.get('name')} {i.get('image_url')}")
