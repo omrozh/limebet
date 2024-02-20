f7deecc70a531f329f4c6073d39aef5044f4585e
omrozh <53612475+omrozh@users.noreply.github.com>
2024-02-20 22:43:35 +0300

    Telegram bot

--- app.py
+++ app.py
@@ -581,7 +581,7 @@ def logout():
 async def telegram_bot():
     if dict(flask.request.json).get("message").get("text") == "/bilgi":
         bot = telegram.Bot("7067705563:AAECNO-7EapKCIvce0xWCR8oIRAlS1N1Uj4")
-        await bot.send_message(text="Selam!")
+        await bot.send_message(text="Selam!", chat_id=dict(flask.request.json).get("message").get("from").get("id"))
     return "OK"
 
 
