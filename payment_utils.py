4254197bb269c82b186ade4f0c7f85d863b72270
omrozh <53612475+omrozh@users.noreply.github.com>
2024-02-22 15:23:47 +0300

    transaction id

--- payment_utils.py
+++ payment_utils.py
@@ -34,7 +34,7 @@ def get_available_amounts(customer_id, bank_code):
 
 def deposit_start(customer_id, customer_name, transaction_id):
     session_id = authenticate(customer_id).get("session_id")
-    banks = get_bank_list("1234", session_id).get("banks")[0].get("bankCode")
+    banks = "ziraat"
     print(get_bank_list("1234", session_id))
     print(get_available_amounts("1234", banks))
     amount = get_available_amounts("1234", banks).get("amounts")[1].get("id")
