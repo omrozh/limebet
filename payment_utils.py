d58892b5d0bff044b6fbbff101c7fb6569b2e240
omrozh <53612475+omrozh@users.noreply.github.com>
2024-02-22 14:48:09 +0300

    bank

--- payment_utils.py
+++ payment_utils.py
@@ -13,4 +13,12 @@ def authenticate(customer_id):
     return r.json()
 
 
+def get_bank_list():
+    session_id = authenticate("12345").get("session_id")
+    r = requests.post("https://test.paygiga.com/api/getBankList", data={
+        "session_id": session_id,
+        "aim": "deposit"
+    })
+
+
 print(authenticate("12345"))
