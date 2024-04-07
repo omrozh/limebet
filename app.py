2a71a370ddc6368e9add8df48d76cb694223c2d5
omrozh <53612475+omrozh@users.noreply.github.com>
2024-04-07 16:58:12 +0300

    unique bet odds

--- app.py
+++ app.py
@@ -414,7 +414,7 @@ class UserInformation(db.Model):
     user_fk = db.Column(db.Integer)
     address = db.Column(db.String)
     date_of_birth = db.Column(db.String)
-    tc_kimlik_no = db.Column(db.Integer)
+    tc_kimlik_no = db.Column(db.Integer, unique=True)
     gender = db.Column(db.String)
     id_verified = db.Column(db.Boolean)
 
