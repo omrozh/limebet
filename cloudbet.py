2216fc6e6ebe705d52fce5cea7c9fa721e366b62
omrozh <53612475+omrozh@users.noreply.github.com>
2024-04-10 15:10:20 +0300

    new

--- cloudbet.py
+++ cloudbet.py
@@ -49,7 +49,7 @@ def get_odds_cloudbet():
                         selections.append(selection)
                 odds.append(
                     {
-                        "gameName": language_dictionary.get(market, market).replace("{", "").replace("}", "").capitalize(),
+                        "gameName": language_dictionary.get(market, market),
                         "gameDetails": "",
                         "odds": [{
                             "gameID": "tbd",
