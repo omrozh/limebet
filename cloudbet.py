23301f0219bf4924ba7eb6f5e3d4f9d0418e4883
omrozh <53612475+omrozh@users.noreply.github.com>
2024-04-10 14:54:22 +0300

    new

--- cloudbet.py
+++ cloudbet.py
@@ -7,7 +7,10 @@ import datetime
 
 
 def get_odds_cloudbet():
-    event_url = f"https://sports-api.cloudbet.com/pub/v2/odds/events?sport=soccer&live=false&limit=10&from={int(time.time())}&to={int(time.time()+3600*3)}"
+    with open("cloudbets_readable_json", "r") as language_data:
+        language_dictionary = json.loads(language_data.read())
+
+    event_url = f"https://sports-api.cloudbet.com/pub/v2/odds/events?sport=soccer&live=false&limit=1000&from={int(time.time())}&to={int(time.time()+3600*24)}"
     api_key = "eyJhbGciOiJSUzI1NiIsImtpZCI6IkhKcDkyNnF3ZXBjNnF3LU9rMk4zV05pXzBrRFd6cEdwTzAxNlRJUjdRWDAiLCJ0eXAiOiJKV1QifQ.eyJhY2Nlc3NfdGllciI6InRyYWRpbmciLCJleHAiOjIwMjgwNDU1ODksImlhdCI6MTcxMjY4NTU4OSwianRpIjoiYjIxZGRjZGYtZjZmNy00NTg1LWFlZDItOTVhNTkwMmU2YmUxIiwic3ViIjoiMzEyMTg1NDgtY2U3MS00NWJiLTlmNTctNmE4YmI3NTI5NjY2IiwidGVuYW50IjoiY2xvdWRiZXQiLCJ1dWlkIjoiMzEyMTg1NDgtY2U3MS00NWJiLTlmNTctNmE4YmI3NTI5NjY2In0.1e8o2kkX_UEccVndkKZDUS0IER6pFJPaSpIR3dzb486PyfpbFq82UggU6goIj9g7hns8sB1HNV__9U6XXLStE_x2qWDd2ZoFwMTeZeuGyMBFqdUK3Z-GGAg-_uYr3wqRB9QbhHHrS_BXEyTpRoxuGLncY8Yq87XuyfH0KbTAjexJOqd6RoseKGLnkex2mAaCc53CrLJh2ysq8wvLtRAYDxCQQN7eCbhRm58TDjTFZKU49u3kokNy4JuwLgjubcqC7F1ibYXwLMGPYH6kSN2zkApje_kmw3SSpJ3HqXptfdy1bIsV-GvlSXStpFnz7btp2Jj2Dhv4E4Hqclt8bRQRng"
 
     event_id = 123456
@@ -46,7 +49,7 @@ def get_odds_cloudbet():
                         selections.append(selection)
                 odds.append(
                     {
-                        "gameName": market,
+                        "gameName": language_dictionary.get(market, market),
                         "gameDetails": "",
                         "odds": [{
                             "gameID": "tbd",
