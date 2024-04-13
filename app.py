99af9e4f6b0d58c0777f710871eba24931b1f0c4
omrozh <53612475+omrozh@users.noreply.github.com>
2024-04-13 19:45:43 +0300

    test live betting

--- app.py
+++ app.py
@@ -23,6 +23,8 @@ import threading
 
 from betting_utils import distribute_rewards, live_betting, instant_odds_update, register_open_bet
 
+schedule.clear()
+
 # schedule.every(3).hours.do(distribute_rewards)
 schedule.every(5).minutes.do(live_betting)
 schedule.every(5).seconds.do(instant_odds_update)
