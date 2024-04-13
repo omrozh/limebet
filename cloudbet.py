48ff70f8b200239df41f0e97636bc88a429f80d5
omrozh <53612475+omrozh@users.noreply.github.com>
2024-04-13 14:29:01 +0300

    all

--- cloudbet.py
+++ cloudbet.py
@@ -90,6 +90,7 @@ def place_bet(bet_odd: BetOdd, reference_id):
         "stake": "0.1"
     }
     response = requests.post(trading_url, headers=headers, data=data)
+    print(response.text)
     return response.json().get("status") == "ACCEPTED" or response.json().get("status") == "PENDING_ACCEPTANCE"
 
 
