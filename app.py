9a77e684bba6904c2f0e122aea188d7e12aae8e0
omrozh <53612475+omrozh@users.noreply.github.com>
2024-04-13 19:31:55 +0300

    test live betting

--- app.py
+++ app.py
@@ -18,12 +18,12 @@ from imap_tools import MailBox
 
 import schedule
 
-# from betting_utils import distribute_rewards, live_betting, instant_odds_update, register_open_bet
+from betting_utils import distribute_rewards, live_betting, instant_odds_update, register_open_bet
 
 # schedule.every(3).hours.do(distribute_rewards)
-# schedule.every(5).minutes.do(live_betting)
-# schedule.every(5).seconds.do(instant_odds_update)
-# schedule.every(24).hours.do(register_open_bet)
+schedule.every(5).minutes.do(live_betting)
+schedule.every(5).seconds.do(instant_odds_update)
+schedule.every(24).hours.do(register_open_bet)
 
 app = flask.Flask(__name__)
 
