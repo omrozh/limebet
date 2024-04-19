6b16284d1c89a882e65ce2566c1edabbe33b0a3e
omrozh <53612475+omrozh@users.noreply.github.com>
2024-04-19 19:55:35 +0300

    new

--- app.py
+++ app.py
@@ -1405,6 +1405,16 @@ def canli_bahis():
     return flask.render_template("bahis/bahis.html", open_bets=open_bets, canli_bahis=True, sports_leagues=sports_leagues)
 
 
+@app.route("/canli_bahis_mobile")
+def canli_bahis_mobile():
+    open_bets = OpenBet.query.filter(OpenBet.bet_ending_datetime <= datetime.datetime.now()).filter_by(live_betting_expired=False).filter_by(has_odds=True).all()
+    sports_leagues = []
+    for i in open_bets:
+        if i.match_league not in sports_leagues:
+            sports_leagues.append(i.match_league)
+    return flask.render_template("bahis/bahis-mobile.html", open_bets=open_bets, canli_bahis=True, sports_leagues=sports_leagues)
+
+
 @app.route("/bahis/mac/<bahis_id>")
 def bahis_mac(bahis_id):
     open_bet = OpenBet.query.get(bahis_id)
