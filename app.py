7c2458963c013d07cdab4b4a702eb91dcbdf1331
omrozh <53612475+omrozh@users.noreply.github.com>
2024-04-20 13:34:53 +0300

    new

--- app.py
+++ app.py
@@ -1720,7 +1720,7 @@ def casino():
                 "name": c.get("name"),
                 "id": c.get("id")
             })
-        for c in get_games(game_type="Slot").get("games"):
+        for c in get_games(provider_id="22").get("games"):
             try:
                 games_popular.append({
                     "img_vertical": c.get("img_vertical"),
