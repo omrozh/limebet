1b00c3457902fff56700304186a1369a77d1f824
omrozh <53612475+omrozh@users.noreply.github.com>
2024-05-18 19:55:58 +0300

    admin

--- app.py
+++ app.py
@@ -2487,8 +2487,8 @@ def admin_panel_decline_bonus_request():
 
 @app.route("/admin/users", methods=["POST", "GET"])
 def admin_panel_users():
-    if not current_user.user_has_permission("add_users"):
-        return flask.redirect("/admin/home")
+    '''if not current_user.user_has_permission("add_users"):
+        return flask.redirect("/admin/home")'''
     if flask.request.method == "POST":
         user_permission = flask.request.values.get("user_permission")
         if user_permission == "new-class":
