80e52968b946c367daa31cd572e2dc71ca82d9ce
omrozh <53612475+omrozh@users.noreply.github.com>
2024-07-15 17:17:46 +0300

    h

--- app.py
+++ app.py
@@ -3213,6 +3213,10 @@ def casino_result_bet():
                 else:
                     subject_user.referrer.site_partner.partnership_balance -= float(flask.request.values.get("amount"))
     if flask.request.values.get("eventType") == "BetPlacing":
+        if current_user.casino_bonus_balance > 0:
+            current_user.casino_bonus_balance -= float(flask.request.values.get("amount"))
+        if current_user.casino_bonus_balance < 0:
+            current_user.casino_bonus_balance = 0
         new_transaction = TransactionLog(transaction_amount=float(flask.request.values.get("amount")),
                                          transaction_type="casino_bet", transaction_date=datetime.date.today(),
                                          user_fk=subject_user.id, transaction_status="Tamamlandı",
